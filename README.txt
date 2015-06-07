2d9ba4183d171374a45c2d6d5871cbcd81243aa5
marcelokk <marcelokkamada@gmail.com>
2015-06-07 15:18:36 -0300

    Botao de proximo e anterior implementados, e escrevi o README

---
+++ README.txt
@@ -0,0 +1,27 @@
+Simulador de Algoritimos para Escalonamento de Processos em Sistemas Interativos.
+
+Autores:
+	Marcelo Koti Kamada		(marcelokkamada@gmail.com)
+	Maria Lydia Fioravanti	()
+	
+# ############### Organizacao do Projeto ###############
+
+raiz:
+	| - > bootstrap (arquivos de css e js para a aparencia das paginas)
+	|
+	| - > engine (motores da simulacao. Cada algoritimo tem o seu proprio diretorio
+	|
+	| - > img (imagens e logos do projeto)
+	|
+	| - > lang (arquivos xml para suporte a diferentes idiomas)
+	|
+	| - > util (arquivos css e jquery para alinhamento das colunas)
+	|
+	| - > index.php (pagina principal, recebe os parametros de simulacao do usuario)
+	|
+	| - > index.js (iteratividade da pagina principal)
+	|
+	| - > simulador.php (pagina de simulacao)
+	|
+	| - > simulador.js (interpreta os parametros de simulacao, executa o motor apropriado e exibe os resultados em simulador.php)
+	
--- simulador.js
+++ simulador.js
@@ -4,6 +4,14 @@ var mensagens = null;
 var estados = null;
 var step = 0;
 
+function clearTable(tabela) {
+	// para cada linha na tabela, exceto a primeira que e' o header
+	var nrows = tabela.rows.length;
+	for(i = 1; i < nrows; i++) {
+        tabela.deleteRow(i);
+	}
+}
+
 function run() {
 	
 	var aux = ['quantum', 'switch_cost', 'io_time', 'processing_until_io'];
@@ -33,7 +41,32 @@ function run() {
 		var n = document.getElementById("status").innerHTML;
 		for(var i = 0; i < n; i++) {
 			var valor = document.getElementById("status" + i).innerHTML;
-			estados.push(valor);
+
+			processos_prontos = new Array();
+			processos_bloqueados = new Array();
+
+			// quebra a string na virgula
+			var texto = valor.split(/,/);
+			var myRegex = /(\w):(\d):(\d)/;
+		
+			for(var k = 0; k < texto.length; k++) {
+				// quebra os processos prontos pelo espaco em branco
+				var tmp = texto[k].split(/\s/);
+						
+				for(var j = 0; j < tmp.length; j++) {
+					if(tmp[j] != "") {
+						var match = myRegex.exec(tmp[j]);
+						var processo = new Array(match[1], match[2], match[3]);
+						
+						if(k == 0) {
+							processos_prontos.push(processo);
+						} else {
+							processos_bloqueados.push(processo);
+						}
+					}
+				}
+			}
+			estados.push([processos_prontos, processos_bloqueados]);
 		}
 	} 
 
@@ -52,50 +85,50 @@ function run() {
     cell1.innerHTML = 'Nome';
     cell2.innerHTML = 'Tempo Restante';
     cell3.innerHTML = 'Estado';
+    
+    reset();
 }
 
 function home() {
 	window.history.back();	
 }
 
+function atualiza() {
+	// atualiza a mensagem
+	document.getElementById("descricao_algoritimo").innerHTML = mensagens[step];
+
+	// atualiza a tabela
+	var tabela = document.getElementById("myTable");
+	clearTable(tabela);
+		
+	// processos prontos
+	var processos_prontos = estados[step][0];
+	for(var i = 0; i < processos_prontos.length; i++) {
+		var row = tabela.insertRow(tabela.rows.length);
+				
+		// cabecalho padrao de todos
+		var cell1 = row.insertCell(0);
+	    var cell2 = row.insertCell(1);
+	    var cell3 = row.insertCell(2);
+			
+	    // Add some text to the new cells:
+	    cell1.innerHTML = processos_prontos[i][0];
+	    cell2.innerHTML = processos_prontos[i][1];
+	    cell3.innerHTML = processos_prontos[i][2];
+	}
+}
+
 function next() {
 	if(step + 1 < mensagens.length) {
 		step = step + 1;
-		document.getElementById("descricao_algoritimo").innerHTML = mensagens[step];
-		
-		var tabela = document.getElementById("myTable");
-
-		// quebra a string na virgula
-		var texto = estados[step].split(/,/);
-		var myRegex = /(\w):(\d):(\d)/;
-
-		for(var i = 0; i < texto.length; i++) {
-			// quebra os processos prontos pelo espaco em branco
-			var tmp = texto[i].split(/\s/);
-			
-			for(var j = 0; j < tmp.length; j++) {
-				if(tmp[j] != "") {
-					var match = myRegex.exec(tmp[j]);
-
-					var row = tabela.insertRow(tabela.rows.length);
-					var cell1 = row.insertCell(0);
-				    var cell2 = row.insertCell(1);
-				    var cell3 = row.insertCell(2);
-
-				    cell1.innerHTML = match[1];
-				    cell2.innerHTML = match[2];
-				    cell3.innerHTML = match[3];
-				}
-			}
-		}
+		atualiza()
 	}
 }
 
 function previous() {
 	if(step > 0) {
 		step = step - 1;
-		document.getElementById("descricao_algoritimo").innerHTML = mensagens[step];
-		document.getElementById('teste').innerHTML = estados[step];
+		atualiza();
 	}
 }
 
@@ -103,5 +136,6 @@ function auto() {
 }
 
 function reset() {
-	
+	step = 0;
+	atualiza();
 }
